e7af626ea0e6b94bdce7637e67e30608d6d137a3
Marko Salinic <marko.salinic@gmail.com>
2020-12-31 14:57:14 +0100

    Introduce DisplayValue property for Card class which gives the correct sign for higher cards; Add test coverage

--- Card.cs
+++ Card.cs
@@ -12,9 +12,38 @@
 		
 		public Suite Suite { get; set; }
 
+		public string DisplayValue
+		{
+			get
+			{
+				if (Value > 1 && Value < 11)
+				{
+					return Value.ToString();
+				}
+				else
+				{
+					switch (Value)
+					{
+						case 11:
+							return "A";
+						case 12:
+							return "J";
+						case 13:
+							return "D";
+						case 14:
+							return "K";
+						case 15:
+							return "@";
+						default:
+							throw new System.ArgumentOutOfRangeException("Value is not allowed.");
+					}
+				}
+			}
+		}
+
 		public override string ToString()
 		{
-			return $"{Value} {Suite}";
+			return $"{DisplayValue}{Suite}";
 		}
 
 

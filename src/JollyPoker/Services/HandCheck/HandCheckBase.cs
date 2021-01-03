89c206fd093a736c0614521204d975229c07c995
Marko Salinic <marko.salinic@gmail.com>
2021-01-03 13:43:48 +0100

    Fix Street combination starting with ace

--- src/JollyPoker/Services/HandCheck/HandCheckBase.cs
+++ src/JollyPoker/Services/HandCheck/HandCheckBase.cs
@@ -17,11 +17,25 @@ namespace JollyPoker.Services.HandCheck
 
 		protected bool IsStreet(Card c1, Card c2, Card c3, Card c4, Card c5)
 		{
-			return
+			return IsGeneralStreet(c1, c2, c3, c4, c5) || IsStreetStartingWithAce(c1, c2, c3, c4, c5);
+		}
+
+		private static bool IsGeneralStreet(Card c1, Card c2, Card c3, Card c4, Card c5)
+		{
+			return 
 				c1.Value == c2.Value - 1 &&
 				c2.Value == c3.Value - 1 &&
 				c3.Value == c4.Value - 1 &&
 				c4.Value == c5.Value - 1;
+		}
+
+		private bool IsStreetStartingWithAce(Card c1, Card c2, Card c3, Card c4, Card c5)
+		{
+			return c1.Value == 2 &&
+				c2.Value == 3 &&
+				c3.Value == 4 &&
+				c4.Value == 5 &&
+				c5.Value == 11;
 		}
 	}
 }
